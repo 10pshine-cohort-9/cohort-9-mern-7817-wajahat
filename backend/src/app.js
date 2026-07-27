c31fc8f439a93d95ec16af37a3efa980258352f6
WajahatNazir01 <wajahatnazirwaraich@gmail.com>
2026-07-27 10:42:04 +0500

    configured sonarcube and pino logger

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const requestLogger = require("./middleware/requestLogger");
+const requestLogger = require("./middlewares/requestLogger");
 
 const app = express();
-app.use(requestLogger);l //we place it here beacuse middlewares work from top to btm 
+app.use(requestLogger); //we place it here beacuse middlewares work from top to btm 
 app.use(express.json());
 app.get('/health', (req, res) => {
   res.status(200).json({
--- backend/src/config/logger.js
+++ backend/src/config/logger.js
@@ -1,7 +1,14 @@
 const pino = require("pino");
+const path = require("path");
+const fs = require("fs");
 
+const logDir = path.join(process.cwd(), "logs");
+
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir, { recursive: true });
+}
 const logger = pino(
-  pino.destination("../../logs/app.log")
+  pino.destination(path.join(logDir, "app.log"))
 );
 
 module.exports = logger;
